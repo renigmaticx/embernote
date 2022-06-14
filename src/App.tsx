17bb36b9d8d0f5d3b68aca8c191d88296361be86
renigmaticx <renielgimeno47@gmail.com>
2022-06-15 04:38:47 +0800

    Implement masonry layout for note cards

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { CssBaseline, Grid, Box } from '@mui/material';
 import CreateNote from './components/CreateNote';
 import Note from './components/Note';
+import Masonry from '@mui/lab/Masonry';
 
 function App() {
   const darkTheme = createTheme({
@@ -27,29 +28,56 @@ function App() {
     }
   });
 
+  const notes = [
+    {
+      title: 'Umbrosa Helicoseus',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec lacus non dolor imperdiet ornare. Sed ut congue lectus. Vestibulum congue nisl sed leo pulvinar dapibus. Morbi arcu velit, consectetur eget placerat in, suscipit ut est. Duis pharetra, odio vel posuere rhoncus, leo velit tempus diam, eget viverra lectus urna eu massa. Pellentesque fermentum nibh eros, ac rutrum urna congue eu. Curabitur rhoncus, arcu non pharetra vehicula, lorem tortor aliquam risus, vel pharetra massa risus interdum tortor. Integer porta porta sapien a porttitor.'
+    },
+    {
+      title: 'Umbrosa Helicoseus',
+      content:
+        'Lorem ipsum generosa adum el salivosa heretica Lorem ipsum dolor sit'
+    },
+    {
+      title: 'Umbrosa Helicoseus',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec lacus non dolor imperdiet ornare. Sed ut congue lectus. Vestibulum congue nisl sed leo pulvinar dapibus. Morbi arcu velit, consectetur eget placerat in, suscipit ut est. Duis pharetra, odio vel posuere rhoncus, leo velit tempus diam, eget viverra lectus urna eu massa.'
+    },
+    {
+      title: 'Umbrosa Helicoseus',
+      content:
+        'Lorem ipsum generosa adum el salivosa heretica Lorem ipsum dolor sit'
+    },
+    {
+      title: 'Umbrosa Helicoseus',
+      content:
+        'Lorem ipsum generosa adum el salivosa heretica Lorem ipsum dolor sit'
+    },
+    {
+      title: 'Umbrosa Helicoseus',
+      content:
+        'Lorem ipsum generosa adum el salivosa heretica Lorem ipsum dolor sit'
+    }
+  ];
+
   return (
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <AppBar />
       <CreateNote />
+
       <Box sx={{ flexGrow: 1, mx: 2 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={3}>
-            <Note />
-          </Grid>
-          <Grid item xs={12} md={3}>
-            <Note />
-          </Grid>
-          <Grid item xs={12} md={3}>
-            <Note />
-          </Grid>
-          <Grid item xs={12} md={3}>
-            <Note />
-          </Grid>
-          <Grid item xs={12} md={3}>
-            <Note />
-          </Grid>
-        </Grid>
+        <Masonry columns={{ xs: 1, sm: 2, md: 4 }} spacing={1}>
+          {notes.map((note, index) => (
+            <Note
+              key={index}
+              id={index}
+              title={note.title}
+              content={note.content}
+            />
+          ))}
+        </Masonry>
       </Box>
     </ThemeProvider>
   );
--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,25 +1,14 @@
-import * as React from 'react';
+import React from 'react';
 import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
 import { CardContent, Typography } from '@mui/material';
 
-function Note() {
+function Note(props: any) {
   return (
     <Card variant="outlined">
       <CardContent>
-        <Typography variant="subtitle2">Umbrosa Helicoseus</Typography>
-        <Typography variant="body2">
-          Lorem ipsum generosa adum el salivosa heretica Lorem ipsum dolor sit
-          amet, consectetur adipiscing elit. In nec lacus non dolor imperdiet
-          ornare. Sed ut congue lectus. Vestibulum congue nisl sed leo pulvinar
-          dapibus. Morbi arcu velit, consectetur eget placerat in, suscipit ut
-          est. Duis pharetra, odio vel posuere rhoncus, leo velit tempus diam,
-          eget viverra lectus urna eu massa. Pellentesque fermentum nibh eros,
-          ac rutrum urna congue eu. Curabitur rhoncus, arcu non pharetra
-          vehicula, lorem tortor aliquam risus, vel pharetra massa risus
-          interdum tortor. Integer porta porta sapien a porttitor.
-        </Typography>
+        <Typography variant="subtitle2">{props.title}</Typography>
+        <Typography variant="body2">{props.content}</Typography>
       </CardContent>
     </Card>
   );
